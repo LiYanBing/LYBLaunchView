962716dfb038c3f7f1a5c9f2baaed09471d20be8
@LiYanBing <@gsliyanbing@163.com>
2016-06-24 16:54:34 +0800

    Initial Commit

---
+++ LYBLaunchView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYBLaunchView
+//
+//  Created by 李彦兵 on 16/6/24.
+//  Copyright © 2016年 Leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
